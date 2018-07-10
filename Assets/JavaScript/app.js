29c87f42a829f152269812932cc1826ce3cdf01e
Twoxbradley <jimmybradley32@gmail.com>
2018-07-09 23:30:18 -0700

    code update

--- Assets/JavaScript/app.js
+++ Assets/JavaScript/app.js
@@ -13,18 +13,18 @@
 // The timer begins a countdown from 60 seconds to 0
 // There are 8 questions total multiple choice questions.  
 
+//RR - Glabal var to store id from call to setInterval for clearing
+var timerId = null;
 
 $(document).ready(function () {
-// on start click ... paragraph fades, questions appear with radio buttons and timer begins (60 secons)
+    // on start click ... paragraph fades, questions appear with radio buttons and timer begins (60 secons)
     $("#startBtn").on("click", function () {
-        $("#para").fadeOut(200, function () {
-            $("#startBtn").hide();
+        $("#start-content").fadeOut(200, function () {
             countdown(60);
-            questions();
+            questionsDisplay();
         });
     });
 });
-
 
 //string of questions, and answers
 var questions = [{
@@ -37,9 +37,9 @@ var questions = [{
     },
     {
         ques: 'Because you know Taekwondo, you have the right to use it anytime you please?',
-        ans: ["true, false"],
+        ans: ['true', 'false'],
         name: 'discipline',
-        correct: false,
+        correct: 'false',
         divClass: ".discipline"
 
     },
@@ -78,9 +78,9 @@ var questions = [{
     },
     {
         ques: 'Taekwondo is both a Martial Art and a combat sport?',
-        ans: ["true, false"],
+        ans: ['true', 'false'],
         name: 'sport',
-        correct: true,
+        correct: 'true',
         divClass: ".sport"
 
     },
@@ -96,82 +96,41 @@ var questions = [{
 
 
 var questionsDisplay = function () {
-    $(".questions :not('#sub-but')").empty();
+    $("#questions").empty();
+
+    //RR - changed loop condition to use questions.length instead of static number, this way when we add/remove questions, it won't break our loop
     // loops through the 10 questions 
-    for (var j = 0; j < 10; j++) {
-        $('.questions').prepend('<div class="' + questions[j].name + '"></div>');
-        $(questions[j].divClass).append('<div class ="ques-title">' + question[j].ques + '</div>');
+    for (var j = 0; j < 7; j++) {
+        $('#questions').append('<div class="' + questions[j].name + '"></div>');
+        $(questions[j].divClass).append('<div class ="ques-title">' + questions[j].ques + '</div>');
         // loops through answers for each radio button
-        for (var i = 0; i <= 3; i++) {
-            $(questions[j].divClass).append('<input type="radio"  name="' + questions[j].name + '" value="' + questions[j].ans[i] + '"/><label for="' + labels[i] + '">' + questions[j].ans[i] + '</label>');
+
+        //get ans array from current questions element
+        const ans = questions[j].ans;
+
+        for (var i = 0; i < ans.length; i++) {
+            $(questions[j].divClass).append('<input type="radio"  name="' + questions[j].name + '" value="' + ans[i] + '"/><label for="' + questions[j].name + '">' + ans[i] + '</label>');
         }
         $('.questions').prepend('<hr />');
     }
+
+    $("#quiz-content").show();
 }
 
-for (var i = 0; i < 10; i++) {
-
-    if ($('input:radio[name="' + questions[i].name + '"]:checked').val() === questions[i].correct) {
-        correctAnswers++;
-        console.log("this is correct! number:" + i)
-    } else {
-        wrongAnswers++;
-        console.log("this is wrong! number:" + i)
-    };
-}
-
-//timer
-var countdown = function (seconds) {
-
-    var timer = setInterval(function () {
-        seconds = seconds - 1;
-        $("#time-remain").html(seconds);
-
-
-        if (seconds <= 0) {
-            $('.container').fadeOut(500);
-            var correctAnswers = 0;
-            var wrongAnswers = 0;
-            var unAnswered = 0;
-
-            for (var i = 0; i < 10; i++) {
-
-                if ($('input:radio[name="' + questions[i].name + '"]:checked').val() === questions[i].correct) {
-
-                    correctAnswers++;
-                    console.log("this is correct! number:" + i)
-                } else {
-                    wrongAnswers++;
-                    console.log("this is wrong! number:" + i)
-                };
-            }
-
-            $('#correctTimesUp').append(correctAnswers);
-            // display wrongAnswers
-            $('#wrongTimesUp').append(wrongAnswers);
-            $('#timesUp').fadeIn(1000).show();
-
-            // alert("Times Up!");
-            clearInterval(timer);
-            return;
-        }
-    }, 1000);
-
-    // click event for submit button to stop timer
-    $('#sub-but').on('click', function () {
-        clearInterval(timer);
-    })
-}; // end countdown
-
-// function to grade quiz once submit button is clicked
-var gradeQuiz = $('#sub-but').on('click', function () {
+//RR - gradeQuiz function will stop timer and loop through questions and count up number of correct/incorrect answers chosen
+var gradeQuiz = function () {
+    // once submit is clicked...
+    // tests
+    // stop timer
+    clearInterval(timerId);
 
     var correctAnswers = 0;
     var wrongAnswers = 0;
     var unAnswered = 0;
 
+    //RR - changed loop condition to use questions.length
     // loop through correctArray & radioName to match html elements & answers
-    for (var i = 0; i < 10; i++) {
+    for (var i = 0; i < questions.length; i++) {
 
         if ($('input:radio[name="' + questions[i].name + '"]:checked').val() === questions[i].correct) {
 
@@ -181,17 +140,43 @@ var gradeQuiz = $('#sub-but').on('click', function () {
         };
     };
 
-    // once submit is clicked...
-    // tests
-    // stop timer
-    countdown();
     // fade out questions
-    $('.container').fadeOut(500);
+    $('#quiz-content').fadeOut(500);
     // show answerScreen
-    $('#answerScreen').show();
+    $('#answer-screen').show();
     // display correctAnswers
-    $('#correctScreen').append(correctAnswers);
+    $('#correct-screen').append(correctAnswers);
     // display wrongAnswers
-    $('#wrongScreen').append(wrongAnswers);
+    $('#wrong-screen').append(wrongAnswers);
+}
 
-}); 
+//RR - will set a countdown for specified number of seconds
+//timer
+var countdown = function (seconds) {
+
+    //RR - store the returned interval id in gloabl var so other functions have access
+    timerId = setInterval(function () {
+        seconds = seconds - 1;
+        $("#time-remain").html(seconds);
+
+        //when time runs out, call grade quiz
+        if (seconds <= 0) {
+            gradeQuiz();
+        }
+    }, 1000);
+
+}; // end countdown
+
+
+
+//RR - changed submit button to just call gradeQuiz
+// function to grade quiz once submit button is clicked
+$('#sub-but').on('click', gradeQuiz);
+
+//resart button click event
+$('#restart-btn').on('click', function () {
+
+    $('#answer-screen').hide();
+    $('#start-content').show();
+
+});
